bcd0284e88b81e497c66b958ce0a167185e8402d
Park-EJ <sophia5091191@gmail.com>
2025-02-25 13:49:13 +0900

    레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,12 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
+    // UserService 클래스에 있는 코드를 요청 DTO에서 처리
     @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."
+    )
     private String newPassword;
 }
